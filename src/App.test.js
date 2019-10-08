8859162bbed1ef338b185a159f7a5adda4ce6bd2
Jeremy Johns <jezjez@gmail.com>
2019-10-09 00:19:53 +0900

    setup jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow((
+      <App />
+    ));
 });
