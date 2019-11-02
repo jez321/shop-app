636fe84cdd9cc5367cbdb0c9a80768195f63fdda
Jeremy Johns <jezjez@gmail.com>
2019-11-02 18:12:02 +0100

    fix App.jsx unit test for redux

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
-
+import store from './redux/store';
+import { Provider } from 'react-redux';
 configure({ adapter: new Adapter() });
 
 const setup = (props = {}, state = null) => {
-  const wrapper = shallow(<App {...props} />);
+  const wrapper = shallow(<Provider store={store}><App {...props} /></Provider>);
   if (state) wrapper.setState(state);
   return wrapper;
 };
