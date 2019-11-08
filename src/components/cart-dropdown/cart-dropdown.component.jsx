e38826d88a01bca86684412e6edebfdedb79e91b
Jeremy Johns <jezjez@gmail.com>
2019-11-08 21:27:43 +0900

    close cart dropdown on go to checkout clicked

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,9 @@ import { connect } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selector';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
@@ -16,7 +17,10 @@ const CartDropdown = ({ cartItems, history }) => (
         )) : <span className="empty-message">Your cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+        }}>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
